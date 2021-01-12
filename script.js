08b6ca629af6945e450fb7196b272c3dfc42e543
ChloeArd <chloe.ardoise@gmail.com>
2021-01-12 14:36:43 +0100

    Exercice 124

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+
+$("button .player__button .toggle").click(function () {
+    player.play();
+})
