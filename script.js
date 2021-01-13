fd924111c4d1e04a3e898686b8df7bb67d0db468
ChloeArd <chloe.ardoise@gmail.com>
2021-01-13 22:01:34 +0100

    Exercice 124

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+let play = $(".player__button").first();
+let playPause = 0;
 
-$("button .player__button .toggle").click(function () {
-    player.play();
-})
+play.click(function () {
+    if (playPause === 0) {
+        player.get(0).play();
+        playPause = 1;
+        play.html("Stop");
+    } else {
+        player.get(0).pause();
+        playPause = 0;
+        play.html("Play");
+    }
+});
+
+player.addEventListener("timeupdate", function (){
+
+});
